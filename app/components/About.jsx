a3df9a780bb79aa822bf758d7ce33c22f2385d2f
Alok kumar <ak0144709@gmail.com>
2026-05-25 17:35:42 +0530

    Enhance About component with user description and skills list, update Tailwind CSS configuration

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import {assets} from '@/assets/assets'
 import Image from 'next/image'
-
+import { infoList } from '@/assets/assets'
 const About = () => {
     return(
         <div className='w-full px-[12%] py-10 scroll-mt-20'>
-            <h4 className='text-center mb-2 text-lg font-Ovo'>Introduction</h4>
-            <h2 className='text-centr text-5xl font-Ovo'>About</h2>
+            <h4 className='text-center mb-2 text-lg font-Ovo'>
+            Introduction</h4>
+            <h2 className='text-centr text-5xl font-Ovo'>
+            About</h2>
 
-            <div>
-                <div>
+            <div className='flex w-full flex-col lg:flex-row items-start gap-20 my-20'>
+                <div className='w-64 sm:w-80 rounded-3xl max-w-none'>
                     <Image src={assets.user_image} alt="user" className='w-full rounded-3xl' />
                 </div>
                 <div className='flex-1'>
+                     <p className='mb-10 max-w-2xl font-Ovo'>
+                        I am a passionate frontend web developer and a student who is continuously improving my skills in web development. 
+                        I enjoy creating modern and responsive web pages using technologies like HTML, CSS, JavaScript, React, and Next.js.
+                        I have started building projects to gain practical experience and enhance my creativity in designing user-friendly websites
+                    </p>
+
+                     <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
+                        {infoList.map(({icon, iconDark, title, description},
+                         index)=>(
+                            <li className='border-[0.5px] border-gray-400
+                             rounded-xl p-6 cursor-pointer hover:bg-lightHover 
+                             hover:-translate-y-1 duration-500 hover:shadow-black'
+                            key={index}>
+                            <Image src={icon} alt={title} className='w-7 mt-3' />
+                            <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
+                            <p className='text-gray-600 text-sm'>{description}</p>
+                            </li>
+                         ))}
+
+                    </ul>
 
                 </div>
             </div>
