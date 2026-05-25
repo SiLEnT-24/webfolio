a5ec32c2513c307ca5f23f1a62c76e3f45e3b19a
Alok kumar <ak0144709@gmail.com>
2026-05-25 14:19:28 +0530

    Enhance About component layout and styling, add user image

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import {assets} from '@/assets/assets'
+import Image from 'next/image'
 
 const About = () => {
     return(
-        <div className='w-full px-[12%]'>
-            <h4>Introduction</h4>
-            <h2>About</h2>
+        <div className='w-full px-[12%] py-10 scroll-mt-20'>
+            <h4 className='text-center mb-2 text-lg font-Ovo'>Introduction</h4>
+            <h2 className='text-centr text-5xl font-Ovo'>About</h2>
+
+            <div>
+                <div>
+                    <Image src={assets.user_image} alt="user" className='w-full rounded-3xl' />
+                </div>
+                <div className='flex-1'>
+
+                </div>
+            </div>
         </div>
     )
 }
