ca874e76c38a4c52253c8489fa2578864c05acc0
Alok kumar <ak0144709@gmail.com>
2026-05-25 18:47:58 +0530

    Add Services component and integrate it into Home page layout; update About component with an ID for navigation

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -6,7 +6,7 @@ import { toolsData } from '@/assets/assets'
 
 const About = () => {
     return(
-        <div className='w-full px-[12%] py-10 scroll-mt-20'>
+        <div id="about" className='w-full px-[12%] py-10 scroll-mt-20'>
             <h4 className='text-center mb-2 text-lg font-Ovo'>
             Introduction</h4>
             <h2 className='text-centr text-5xl font-Ovo'>
---
+++ app/components/Services.jsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+import { assets } from '@/assets/assets'
+import { serviceData } from '@/assets/assets'
+import React from 'react'
+
+const Services = ()=>{
+    return(
+        <div id="sevices" className='w-full px-[12%] py-10 scroll-mt-20'>
+            <h4 className='text-center mb-2 text-lg font-Ovo'>
+            What I offer</h4>
+            <h2 className='text-center text-5xl font-Ovo'>
+            My Services</h2>
+
+            <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+                I am a frontend web developer 
+                and a student passionate about creating modern and responsive websites.
+            </p>
+
+            <div className='grid grid-cols-4 gap-6 my-10'>
+                {serviceData.map(({icon, title, description, link}, index)=>(
+                    <div key={index}>
+                        <Image src={icon} alt='' className='w-10' />
+                        <h3 className='text-lg my-4 text-gray-700 '>{title}</h3>
+                        <p className='text-sm text-gray-600 leading-5'>{description}</p>
+                        <a href={link} className='flex items-center gap-2 text-sm mt-5'>
+                         Read more <Image src={assets.right_arrow} className='w-4' />
+                        </a>
+                    </div>
+                        
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Services
