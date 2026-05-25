876c5463e59b7d15bb84dde8e4a1b6c0dc9bef08
Alok kumar <ak0144709@gmail.com>
2026-05-25 19:49:04 +0530

    Add Work component to display projects with dynamic data and styling

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -1,3 +1,6 @@
+import Image from 'next/image'
+import { assets } from '@/assets/assets'
+import { workData } from '@/assets/assets'
 import React from 'react'
 
 const Work = () =>{
@@ -11,7 +14,26 @@ const Work = () =>{
             <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
                Welcome to my Web development Webfolio! Explore a collection of projects. 
             </p>
-
+        
+        <div className='grid grid-cols-auto my-10 gap-5'>
+            {workData.map((project, index)=>(
+                <div className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer '
+                 key={index} style={{backgroundImage: `url(${project.bgImage})`}}>
+                    <div className='bg-white w-10/12 rounded-md 
+                    absolute bottom-5 left-1/2 -translate-x-1/2 py-5 
+                    px-3 flex items-center justify-between duration-500 group-hover:bottom-7'>
+                        <div>
+                            <h2 className='font-semibold'>{project.title}</h2>
+                            <p className='text-sm text-gray-700'>{project.description}</p>
+                        </div>
+                        <div className='border rounded-full border-black w-9 aspect-square 
+                        flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition' >
+                           <Image src={assets.send_icon} alt='send icon' className='w-5' />
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
         </div>
     )
 }
