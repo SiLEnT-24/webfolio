567d49e9d7504f6658150ea1bffa430faaa3aeab
Alok kumar <ak0144709@gmail.com>
2026-05-26 11:08:28 +0530

    Update Twitter link in Footer component for accuracy

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = ({isDarkMode}) =>{
                 <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
                     <li><a target='_blank' href="">Github</a></li>
                     <li><a target='_blank' href="https://www.linkedin.com/in/alok-kumar-8a6350281?utm_source=share_via&utm_content=profile&utm_medium=member_android">LinkedIn</a></li>
-                    <li><a target='_blank' href="">Twitter</a></li>
+                    <li><a target='_blank' href="https://x.com/Alok_2410">Twitter</a></li>
                 </ul>
             </div>
 
