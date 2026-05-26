70eee9d89ae5db43333c3eafe126282b12ee3165
Alok kumar <ak0144709@gmail.com>
2026-05-26 22:17:29 +0530

    Refactor Navbar component to improve scroll event handling and enhance background opacity for better visibility

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -15,19 +15,25 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
         sideMenuRef.current.style.transform = 'translateX(16rem)'
     }
 
-    useEffect(()=>{
-        window.addEventListener('scroll', ()=>{
-            if(scrollY > 50){
-                setIsScroll(true)
-            }else{
+    useEffect(() => {
+
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+               setIsScroll(true)
+            } else {
                 setIsScroll(false)
-            }
-        })
-    },[])
+           }
+       }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => window.removeEventListener('scroll', handleScroll)
+
+     }, [])
 
     return(
         <>
-        <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden'>
+        <div className='fixed top-0 right-0 w-11/12 -z-10 opacity-50 dark:hidden'>
             <Image src={assets.header_bg_color} alt="" className='w-full' />
         </div>
           <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%]  
@@ -38,7 +44,7 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
             </a>
 
             <ul className={`hidden md:flex items-center
-             gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white ${isScroll ? "" : "shadow-sm bg-opacity-50 dark:border dark:border-white/50 dark:bg-transparent "}`}>
+             gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "bg-white dark:bg-darkTheme shadow-sm" : "shadow-sm bg-opacity-50 dark:border dark:border-white/50 dark:bg-transparent "}`}>
                 <li><a className='font-Ovo' href="#top">Home</a></li>
                 <li><a className='font-Ovo' href="#about">About</a></li>
                 <li><a className='font-Ovo' href="#services">Services</a></li>
